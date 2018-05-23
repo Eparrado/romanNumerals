46621be103eb7ca315881d6f9eb84af8ed5b532f
Estela Parrado <estela.parrado@gmail.com>
2018-05-23 10:57:05 +0200

    REFACTOR create pattern module

--- __tests__/romanNumerals.spec.js
+++ __tests__/romanNumerals.spec.js
@@ -1,13 +1,17 @@
+const RomanNumerals = require('../src/romanNumerals');
+let app;
 describe('romanNumerals', function () {
+
+    beforeEach(function () {
+        app = RomanNumerals();
+    });
+
     it('converts numbers to roman numerals', function () {
-        var RomanNumerals = require('../src/romanNumerals');
-
-        expect(RomanNumerals.fromNumber(1)).toBe('I');
-        expect(RomanNumerals.fromNumber(2)).toBe('II');
-        expect(RomanNumerals.fromNumber(3)).toBe('III');
-        expect(RomanNumerals.fromNumber(4)).toBe('IV');
-        expect(RomanNumerals.fromNumber(2648)).toBe('MMDCXLVIII');
-
+        expect(app.fromNumber(1)).toBe('I');
+        expect(app.fromNumber(2)).toBe('II');
+        expect(app.fromNumber(3)).toBe('III');
+        expect(app.fromNumber(4)).toBe('IV');
+        expect(app.fromNumber(2648)).toBe('MMDCXLVIII');
     });
 
 });
--- src/romanNumerals.js
+++ src/romanNumerals.js
@@ -1,36 +1,48 @@
-var RomanNumerals = function () { };
+function RomanNumerals() {
 
-var numeral = function (value, numeral) {
-    return {
-        value,
-        numeral
-    }
-}
-
-var numerals = [
-    numeral(1000, 'M'),
-    numeral(900, 'CM'),
-    numeral(500, 'D'),
-    numeral(400, 'CD'),
-    numeral(100, 'C'),
-    numeral(90, 'XC'),
-    numeral(50, 'L'),
-    numeral(40, 'XL'),
-    numeral(10, 'X'),
-    numeral(9, 'IX'),
-    numeral(5, 'V'),
-    numeral(4, 'IV'),
-    numeral(1, 'I')
-]
-
-RomanNumerals.prototype.fromNumber = function (n) {
-    result = '';
-    numerals.forEach(function (item) {
-        for (; n >= item.value; n -= item.value) {
-            result += item.numeral;
+    var numeral = function (value, numeral) {
+        return {
+            value,
+            numeral
         }
-    });
-    return result;
+    }
+
+    var numerals = [
+        numeral(1000, 'M'),
+        numeral(900, 'CM'),
+        numeral(500, 'D'),
+        numeral(400, 'CD'),
+        numeral(100, 'C'),
+        numeral(90, 'XC'),
+        numeral(50, 'L'),
+        numeral(40, 'XL'),
+        numeral(10, 'X'),
+        numeral(9, 'IX'),
+        numeral(5, 'V'),
+        numeral(4, 'IV'),
+        numeral(1, 'I')
+    ]
+
+    function fromNumber(n) {
+        result = '';
+        numerals.forEach(function (item) {
+            for (; n >= item.value; n -= item.value) {
+                result += item.numeral;
+            }
+        });
+        return result;
+    }
+
+
+    return {
+        fromNumber
+    }
+};
+
+
+
+if (typeof (module) != undefined) {
+    module.exports = RomanNumerals;
 }
 
-module.exports = new RomanNumerals;
+
